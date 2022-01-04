f01edb164826da9e0b641c0d9b15824af8b458a4
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-01-04 23:49:56 +0700

    getStaticProps

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import fs from "fs/promises";
+import path from "path";
+
+const HomePage = (props) => {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', "dummy-backend.json")
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
+
+export default HomePage;
