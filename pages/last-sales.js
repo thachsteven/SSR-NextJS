fcaf54091c521ca2e99fa4274721aaf6a0ce9e4b
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-01-05 15:23:32 +0700

    getStaticProps in last-sales component

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState();
-  //   const [isLoading, setIsLoading] = useState(false);
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales);
 
   const { data, error } = useSWR(
     "https://next-js-course-b4ec5-default-rtdb.firebaseio.com/sales.json",
     (url) => fetch(url).then((res) => res.json())
   );
-
-  console.log(data);
 
   useEffect(() => {
     if (data) {
@@ -27,31 +24,11 @@ const LastSalesPage = () => {
     }
   }, [data]);
 
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //     fetch("https://next-js-course-b4ec5-default-rtdb.firebaseio.com/sales.json")
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const transformedSales = [];
-
-  //         for (const key in data) {
-  //           transformedSales.push({
-  //             id: key,
-  //             username: data[key].username,
-  //             volume: data[key].volume,
-  //           });
-  //         }
-
-  //         setSales(transformedSales);
-  //         setIsLoading(false);
-  //       });
-  //   }, []);
-
   if (error) {
     return <p>Fail to load</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -65,5 +42,26 @@ const LastSalesPage = () => {
     </ul>
   );
 };
+
+export async function getStaticProps(context) {
+
+    const response = await fetch("https://next-js-course-b4ec5-default-rtdb.firebaseio.com/sales.json")
+        const data = await response.json();
+          const transformedSales = [];
+
+          for (const key in data) {
+            transformedSales.push({
+              id: key,
+              username: data[key].username,
+              volume: data[key].volume,
+            });
+          }
+
+          return {
+              props: {sales: transformedSales},
+            //   revalidate: 10
+          }
+
+}
 
 export default LastSalesPage;
